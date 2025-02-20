cd981c97bcf7834c2ea4294afdcabc056a75cc4c
yangziyiiii <2420769218@qq.com>
2025-02-20 11:07:08 +0800

    update cvc5 deps and bitwuzla deps

--- contrib/setup-cvc5.sh
+++ contrib/setup-cvc5.sh
@@ -6,7 +6,7 @@ DEPS=$DIR/../deps
 
 mkdir -p $DEPS
 
-CVC5_VERSION=cvc5-1.1.1
+# CVC5_VERSION=cvc5-1.1.1
 
 if [ "$(uname)" == "Darwin" ]; then
     NUM_CORES=$(sysctl -n hw.logicalcpu)
@@ -20,19 +20,22 @@ $DIR/setup-cadical.sh
 
 if [ ! -d "$DEPS/cvc5" ]; then
     cd $DEPS
-    git clone https://github.com/cvc5/cvc5.git
-    chmod -R 777 cvc5
-    cd cvc5
-    git checkout -f ${CVC5_VERSION}
-    CXXFLAGS=-fPIC CFLAGS=-fPIC ./configure.sh --static --auto-download --dep-path="$DEPS/install"
-    cd build
-    make -j$NUM_CORES
+    # git clone https://github.com/cvc5/cvc5.git
+    # chmod -R 777 cvc5
+    # cd cvc5
+    # git checkout -f ${CVC5_VERSION}
+    # CXXFLAGS=-fPIC CFLAGS=-fPIC ./configure.sh --static --auto-download --dep-path="$DEPS/install"
+    # cd build
+    # make -j$NUM_CORES
+    wget https://github.com/cvc5/cvc5/releases/download/cvc5-1.1.2/cvc5-Linux-static.zip
+    unzip cvc5-Linux-static.zip -d .
+    mv cvc5-Linux-static cvc5
     cd $DIR
 else
     echo "$DEPS/cvc5 already exists. If you want to rebuild, please remove it manually."
 fi
 
-if [ -f $DEPS/cvc5/build/src/libcvc5.a ] && [ -f $DEPS/cvc5/build/src/parser/libcvc5parser.a ] && [ -f $DEPS/install/lib/libcadical.a ]; then
+if [ -f $DEPS/cvc5/lib/libcvc5.a ] && [ -f $DEPS/cvc5/lib/libcvc5parser.a ] && [ -f $DEPS/lib/libcadical.a ]; then
     echo "It appears cvc5 was setup successfully into $DEPS/cvc5."
     echo "You may now install it with make ./configure.sh --cvc5 && cd build && make"
 else
