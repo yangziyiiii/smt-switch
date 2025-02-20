4cd22c94887926ff349f85a23965cc528a556255
yangziyiiii <2420769218@qq.com>
2025-02-20 11:35:41 +0800

    bitwuzla link success

--- tests/test-bv.cpp
+++ tests/test-bv.cpp
@@ -14,45 +14,84 @@
 **
 **/
 
-#include <utility>
-#include <vector>
+// #include <utility>
+// #include <vector>
 
-#include "available_solvers.h"
-#include "gtest/gtest.h"
+// #include "available_solvers.h"
+// #include "gtest/gtest.h"
+// #include "smt.h"
+
+// using namespace smt;
+// using namespace std;
+
+// namespace smt_tests {
+
+// GTEST_ALLOW_UNINSTANTIATED_PARAMETERIZED_TEST(BVTests);
+// class BVTests : public ::testing::Test,
+//                  public ::testing::WithParamInterface<SolverConfiguration>
+// {
+//  protected:
+//   void SetUp() override
+//   {
+//     s = create_solver(GetParam());
+//     s->set_opt("produce-models", "true");
+//     s->set_opt("incremental", "true");
+//   }
+//   SmtSolver s;
+// };
+
+// // based on issue #308
+// TEST_P(BVTests, to_int)
+// {
+//   Sort sort1 = s->make_sort(BV, 1);
+//   Sort sort2 = s->make_sort(BV, 2);
+//   Term x1 = s->make_symbol("x1", sort1);
+//   Term x2 = s->make_symbol("x2", sort2);
+//   s->check_sat();
+//   uint64_t i1 = s->get_value(x1)->to_int();
+//   uint64_t i2 = s->get_value(x2)->to_int();
+//   ASSERT_TRUE(0 <= i1 && i1 <= 1);
+//   ASSERT_TRUE(0 <= i2 && i2 <= 3);
+
+// }
+
+// INSTANTIATE_TEST_SUITE_P(
+//     ParameterizedSolverBVTests,
+//     BVTests,
+//     testing::ValuesIn(filter_solver_configurations({ THEORY_BV })));
+
+// }  // namespace smt_tests
+
+#include <iostream>
+#include <memory>
+#include <vector>
+#include "assert.h"
+
+#include "bitwuzla_factory.h"
 #include "smt.h"
+// after a full installation
+// #include "smt-switch/boolector_factory.h"
+// #include "smt-switch/smt.h"
 
 using namespace smt;
 using namespace std;
 
-namespace smt_tests {
-
-GTEST_ALLOW_UNINSTANTIATED_PARAMETERIZED_TEST(BVTests);
-class BVTests : public ::testing::Test,
-                 public ::testing::WithParamInterface<SolverConfiguration>
+int main()
 {
- protected:
-  void SetUp() override
+  SmtSolver s = BitwuzlaSolverFactory::create(false);
+  s->set_logic("QF_ABV");
+  s->set_opt("produce-models", "true");
+  Sort bvsort8 = s->make_sort(BV, 8);
+  Term x = s->make_symbol("x", bvsort8);
+  Term y = s->make_symbol("y", bvsort8);
+  Term xpy = s->make_term(BVAdd, x, y);
+
+  for (auto c : xpy)
   {
-    s = create_solver(GetParam());
-    s->set_opt("produce-models", "true");
-    s->set_opt("incremental", "true");
+    assert(c == x || c == y);
   }
-  SmtSolver s;
-};
 
-// based on issue #308
-TEST_P(BVTests, to_int)
-{
-  Sort sort1 = s->make_sort(BV, 1);
-  Sort sort2 = s->make_sort(BV, 2);
-  Term x1 = s->make_symbol("x1", sort1);
-  Term x2 = s->make_symbol("x2", sort2);
-  s->check_sat();
-  uint64_t i1 = s->get_value(x1)->to_int();
-  uint64_t i2 = s->get_value(x2)->to_int();
-  ASSERT_TRUE(0 <= i1 && i1 <= 1);
-  ASSERT_TRUE(0 <= i2 && i2 <= 3);
-
+<<<<<<< HEAD
   Sort bv3 = s->make_sort(BV, 3);
   Term minus_one = s->make_term(-1, bv3);
   Term one = s->make_term(1, bv3);
@@ -61,11 +100,7 @@ TEST_P(BVTests, to_int)
   s->assert_formula(s->make_term(BVSlt, x, one));
   s->check_sat();
   EXPECT_EQ(s->get_value(x)->to_int(), 0);
+=======
+  return 0;
+>>>>>>> bitwuzla link success
 }
-
-INSTANTIATE_TEST_SUITE_P(
-    ParameterizedSolverBVTests,
-    BVTests,
-    testing::ValuesIn(filter_solver_configurations({ THEORY_BV })));
-
-}  // namespace smt_tests
